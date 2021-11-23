750ad95b99c1d982c2d8b7662ba886d49294eb3a
aloisiot <aloisiot.c98@gmail.com>
2021-11-23 15:37:08 -0300

    primeiro commit

---
+++ src/main/java/dev/aloisiot/jenkins/jenkinsfirststeps/JenkinsFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package dev.aloisiot.jenkins.jenkinsfirststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFirstStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFirstStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aloisiot/jenkins/jenkinsfirststeps/JenkinsFirstStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aloisiot.jenkins.jenkinsfirststeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFirstStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
